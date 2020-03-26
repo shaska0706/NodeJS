c5aa8109547ba2537b78542212f7b2f96848f6bf
shaska0706 <pdiaz07@alumnos.uaq.mx>
2020-03-26 12:09:18 -0600

    AccesoDatos_RegEx

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const app = express();
+const {pokemon} = require("./pokedex.json");
 
 app.get('/', (req,res,next)=> {
-    res.send('Bienvenido');
+    res.send("Bienvenido al Pokedex");
 })
 
-app.get('/:name', (req, res, next)=>{
+app.get('/pokemon/all', (req, res, next)=>{
     console.log(req.params.name);
     res.status(200);
-    res.send('Hola '+req.params.name);
+    res.send(pokemon);
 });  
 
+app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=>{
+    const id = req.params.id -1;
+    if(id >=0 && id <= 150){
+        res.status(200);
+        return res.send(pokemon[req.params.id -1]);
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
+app.get('/pokemon/:name',(req,res,next)=>{
+    const name=req.params.name;
+    for(i=0; i<pokemon.length; i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
 // req=request solicitud del usuario
 // res=respose respuesta que se le da al usuario
 // next=
