bdf6f3aa1251344d6dbc8f8013e96ff71a919513
shaska0706 <pdiaz07@alumnos.uaq.mx>
2020-04-21 12:57:11 -0500

    CambioBodyParser_Code404

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,30 +8,23 @@ pokemon.post("/", (req, res, next)=>{
 
 pokemon.get('/', async (req, res, next)=>{
     const pkmn = await db.query('SELECT * FROM pokemon');
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 });  
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next)=>{
     const id = req.params.id;
     const pok_id = await db.query('SELECT * FROM pokemon WHERE pok_id = ?', id);
     (id >= 1 && id <= 722) ?
-        res.status(200).json(pok_id):
-        res.status(404).send('Pokemon no encontrado');
+        res.status(200).json({code: 1, message: pok_id}):
+        res.status(404).json({code: 404, message: 'Pokemon no encontrado'});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req,res,next)=>{
     const name=req.params.name;
     const pok_name = await db.query('SELECT * FROM pokemon WHERE pok_name =?', name);
     (name.length > 0) ?
-        res.status(200).json(pok_name) : 
-        res.status(404).send("Pokemon no encontrado");
-});
-
-pokemon.get('/:id([0-9]{1,9})', async (req, res, next)=>{
-    const id = req.params.id;
-    if(id >722){
-        res.status(404).send('Pokemon no encontrado');
-    }
+        res.status(200).json({code: 1, message: pok_name}):
+        res.status(404).json({code: 404, message: 'Pokemon no encontrado'});
 });
 
 module.exports = pokemon;
