4c080c2d4ecf1886354e17830300f5503e59e6d7
shaska0706 <pdiaz07@alumnos.uaq.mx>
2020-04-26 03:09:25 -0500

    Añadir_post_a_pokemon

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req,res,next)=> {
-    return res.status(200).send("Bienvenido al Pokedex");
+    return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"});
 });
 
 app.use("/pokemon", pokemon);
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,20 +2,33 @@ const express = require('express');
 const pokemon = express.Router();
 const db = require('../config/database')
 
-pokemon.post("/", (req, res, next)=>{
-    return res.status(200).send(req.body);
-})
+pokemon.post("/", async(req, res, next)=>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += ` VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+        const rows = await db.query(query);
+
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code: 201, message: "Pokemon insertado correctamente"});
+        }
+
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});
+    }
+
+    return res.status(500).json({code: 500, message: "Campos incompletos"});    
+});
 
 pokemon.get('/', async (req, res, next)=>{
     const pkmn = await db.query('SELECT * FROM pokemon');
-    return res.status(200).json({code: 1, message: pkmn});
+    return res.status(200).json({code: 200, message: pkmn});
 });  
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next)=>{
     const id = req.params.id;
     const pok_id = await db.query('SELECT * FROM pokemon WHERE pok_id = ?', id);
     (id >= 1 && id <= 722) ?
-        res.status(200).json({code: 1, message: pok_id}):
+        res.status(200).json({code: 200, message: pok_id}):
         res.status(404).json({code: 404, message: 'Pokemon no encontrado'});
 });
 
@@ -23,7 +36,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req,res,next)=>{
     const name=req.params.name;
     const pok_name = await db.query('SELECT * FROM pokemon WHERE pok_name =?', name);
     (name.length > 0) ?
-        res.status(200).json({code: 1, message: pok_name}):
+        res.status(200).json({code: 200, message: pok_name}):
         res.status(404).json({code: 404, message: 'Pokemon no encontrado'});
 });
 
