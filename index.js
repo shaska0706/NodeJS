6baac4e995f98be3eab23d8e300082831300ec84
shaska0706 <pdiaz07@alumnos.uaq.mx>
2020-05-17 02:28:58 -0500

    Integrando_UI1

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+//Dependencies
 const morgan = require('morgan');
 const express = require("express");
 const app = express();
+//Routes
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
+//Middleware
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
-const index = require('./middleware/index')
+const index = require('./middleware/index');
+const cors =require('./middleware/cors');
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', index);
-
 app.use("/user", user);
-
 app.use(auth);
-
 app.use("/pokemon", pokemon);
-
 app.use(notFound);
 
 app.listen(process.env.Port || 3000, () => {
